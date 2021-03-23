c47389adad20adf6ce4ecb956fe06e8162f89757
jiangzhipeng <570981884@qq.com>
2021-03-23 18:22:37 +0800

    二分查找

--- 16_binary_tree.py
+++ 16_binary_tree.py
@@ -29,3 +29,26 @@ class Tree(object):
                 return
             else:
                 queue.append(cur_node.rchild)
+
+    def breadth_travel(self):
+        '''广度优先遍历'''
+        if self.root is None:
+            return
+        quque = [self.root]
+        while quque:
+            cur_node = quque.pop(0)
+            print(cur_node.elem)
+            if cur_node.lchild is not None:
+                quque.append(cur_node.lchild)
+            if cur_node.rchild is not None:
+                quque.append(cur_node.rchild)
+
+
+if __name__ == "__main__":
+    tree = Tree()
+    tree.add(1)
+    tree.add(2)
+    tree.add(3)
+    tree.add(4)
+    tree.add(5)
+    tree.breadth_travel()
